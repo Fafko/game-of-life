f48381af693a0bbd89f1ed152e904f7edf3c20e1
Alex Panchenko <fafko93@gmail.com>
2017-07-10 00:32:17 +0300

    Fix wrong cell toggling in 'Canvas Renderer'.

--- js/app.js
+++ js/app.js
@@ -2584,8 +2584,8 @@ var CanvasRenderer = function (_AbstractRenderer) {
     key: '_getPointCoordsByEventCoords',
     value: function _getPointCoordsByEventCoords(x, y) {
       return {
-        x: Math.ceil(x / this.POINT_WIDTH) - 1,
-        y: Math.ceil(y / this.POINT_HEIGHT) - 1
+        x: Math.ceil(x / this.POINT_WIDTH * this.SCALE_RATIO) - 1,
+        y: Math.ceil(y / this.POINT_HEIGHT * this.SCALE_RATIO) - 1
       };
     }
 
